55766d82fa8c9f67f1e9664f2b0884ebf5297f25
emre can yılmaz <ecylmz@bil.omu.edu.tr>
2011-11-05 12:11:09 +0200

    reset

--- _scripts/gist-api.py
+++ _scripts/gist-api.py
@@ -49,6 +49,7 @@ def git_submodule():
         for j in range(len(id_map[id_map.keys()[i]])):
             os.system("git submodule add git://gist.github.com/%d.git %d" %
                       (id_map[id_map.keys()[i]][j],id_map[id_map.keys()[i]][j]))
+    os.system("git commit -a -m 'güncellendi.'")
 
 def sub_page():
     template = TemplateManager().prepare(main_path + "/_scripts/templates/sub_template.tmpl")
